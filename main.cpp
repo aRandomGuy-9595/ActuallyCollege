56580d6b7beb941cb89c9197b91561aeb36549e3
poi69420 <74394510+aRandomGuy-9595@users.noreply.github.com>
2025-11-02 16:47:47 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,84 +1,22 @@
-#include <iostream>
-using namespace std;
+#include <SFML/Graphics.hpp>
 
-class Basic_stat{
-private:
-    int hp;
-    int def;
-    int atk;
-    
-public:
-    Basic_stat(int newhp, int newdef, int newatk){
-        hp = newhp;
-        def = newdef;
-        atk = newatk;
-    }
-    int getdef(){
-        return def;
-    }
-    int gethp(){
-        return hp;
-    }
-    int getatk(){
-        return atk;
-    }
-    
-    void damage(int dmg) {
-    hp -= dmg;
-    if (hp < 0) hp = 0;
-    }
+int main() {
+	sf::RenderWindow window(sf::VideoMode({500, 500}), "Particle Simulator :: Rayhan - 5024241032");
+	
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
 
-    virtual void displayInfo(){
-        cout << "Current HP : " << hp << endl;
-        cout << "Current DEF : " << def << endl;
-        cout << "Current ATK : " << atk << endl;
-    }
-};
+		window.clear();
+		window.display();
+	}
 
-class greatsword : public Basic_stat{
-private:
-    int atk;
-    
-public:
-    greatsword(int newhp, int newdef, int newatk) : Basic_stat(newhp, newdef, newatk), atk(newatk) {}
-    
-    int getatk2() {
-        return atk;
-    }
-    
-    void displayInfo() override {
-        cout << "Current HP : " << gethp() << endl;
-        cout << "Current DEF : " << getdef() << endl;
-        cout << "Current ATK : " << atk << endl;
-    }
-
-    void slash(Basic_stat& target) {
-        int damage = atk - target.getdef();
-        if (damage < 0) damage = 0;
-        target.damage(damage);
-        cout << "Sunshine's HP reduced by " << damage << " pts." << endl;
-    }
-
-};
-
-int main(){
-    greatsword supaniga(100, 2, 30);
-    Basic_stat lebron(50, 5, 10); 
-
-    cout << "Alice's Attributes : " << endl; 
-    supaniga.displayInfo(); 
-    cout << endl; 
-
-    cout << "Sunshine's Stats : " << endl; 
-    lebron.displayInfo(); 
-    cout << endl;
-
-    cout << "*Insert Slash SFX*" << endl;
-    supaniga.slash(lebron); 
-    cout << endl;
-
-    cout << "Sunshine's Stats : " << endl; 
-    lebron.displayInfo(); 
-
-    return 0;
+	return 0;
 }
